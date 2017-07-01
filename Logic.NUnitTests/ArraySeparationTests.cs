5d839d4a5019bec2e87abc309beac48822c7398e
adaniluk <allldaniluk@gmail.com>
2017-07-01 12:42:56 +0300

    Added class 'ArraySeparation'

---
+++ Logic.NUnitTests/ArraySeparationTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic;
+
+namespace Logic.NUnitTests
+{
+    [TestFixture]
+    public class ArraySeparationTests
+    {
+        [TestCase(new int[] {1,2,3,4,3,2,1}, ExpectedResult = 3)]
+        [TestCase(new int[] { 1, 2, 3, 4, 3, 2, 1, 0, 0 }, ExpectedResult = 3)]
+        [TestCase(new int[] { 1, 100, 50, -51, 1, 1 }, ExpectedResult = 1)]
+        [TestCase(new int[] { 1, 2, 3, 4, 3, 2 }, ExpectedResult = -1)]
+        public int FindSeparatorIndex_CorrectInputValues_PositiveTest(int[] array)
+        {
+            return ArraySeparation.FindSeparatorIndex(array);
+        }
+
+        [TestCase(new int[] { })]
+        public void FindSeparatorIndex_InputArrayIsEmpty_ThrowsArgumentException(int[] array)
+        {
+            Assert.Throws<ArgumentException>(() => ArraySeparation.FindSeparatorIndex(array));
+        }
+
+        [TestCase(null)]
+        public void FindSeparatorIndex_InputArrayIsNull_ThrowsArgumentNullException(int[] array)
+        {
+            Assert.Throws<ArgumentNullException>(() => ArraySeparation.FindSeparatorIndex(array));
+        }
+    }
+}
---
+++ Logic/ArraySeparation.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Logic
+{
+    public class ArraySeparation
+    {
+        #region FindSeparatorIndex (public)
+        /// <summary>
+        /// Method search an index of the element,for which the sum of the right elements 
+        /// is equal of the sum of the left elements.
+        /// </summary>
+        /// <param name="array">Array of int numbers.</param>
+        /// <returns>Index of found element (returns -1, if element doesn't exist).</returns>
+        public static int FindSeparatorIndex(int[] array)
+        {
+            CheckArray(array);
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                if (FindSum(array, 0, i - 1) == FindSum(array, i + 1, array.Length - 1))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        #endregion
+
+        #region FindSeparatorIndex Helpers (private)
+        /// <summary>
+        /// Method finds the sum of elements in int array from startPosition to finishPosition.
+        /// </summary>
+        /// <param name="array">Array of int numbers.</param>
+        /// <param name="startPosition">Index in array, from which the method will calculate the sum.</param>
+        /// <param name="finishPosition">Index in array, to which the method will calculate the sum.</param>
+        /// <returns>Sum of elements in array.</returns>
+        private static int FindSum(int[] array, int startPosition, int finishPosition)
+        {
+            int result = 0;
+            for (int i = startPosition; i <= finishPosition; i++)
+            {
+                result += array[i];
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Methos checks the input array. 
+        /// </summary>
+        /// <param name="array">Array of int elements.</param>
+        private static void CheckArray(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException("Array is null.");
+            }
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Array is empty.");
+            }
+            if (array.Length >= 1000)
+            {
+                throw new ArgumentException("Array is unsuitable.");
+            }
+        }
+    }
+    #endregion
+}
